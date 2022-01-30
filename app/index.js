6fa702fdb351786fb07b82e0039b55f74f876f37
nanggi4 <nkhun1006@gmail.com>
2022-01-30 23:18:18 +0900

    update new lecture

--- app/index.js
+++ app/index.js
@@ -5,19 +5,29 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/\/+|\/+$/g, '');
   const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headers = req.headers;
 
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
 
+  res.end(' *** Hello World\n');
 
-  res.end('Hello World\n');
-
-  console.log(`Request received on path ${trimmedPath} !!`);
-  console.log(`Request method is ${method} !!`);
+  console.log(`* Request received on path ${trimmedPath}.`);
+  console.log(`** Request method is ${method}.`);
+  console.log(`*** Request query string params is ${JSON.stringify(queryStringObject)}.`);
+  console.log(`**** Request headers is ${headers}.`);
+  console.log(`***** Request payload is ${buffer}.`);
 });
 
 server.listen(3000, () => {
